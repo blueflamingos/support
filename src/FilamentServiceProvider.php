d9d2b4c3ba91a8aa6677d00f354ec9c4d315792b
Robert-John van Doesburg <robert-john@blueflamingos.nl>
2023-08-19 11:41:20 +0200

    Remove registration of horizon user menu

--- src/FilamentServiceProvider.php
+++ src/FilamentServiceProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Blueflamingos\Support;
 
-use Filament\Navigation\UserMenuItem;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -16,15 +15,5 @@ class FilamentServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasConfigFile('blue-flamingos')
             ->hasRoutes('web');
-    }
-
-    protected function getUserMenuItems(): array
-    {
-        return [
-            UserMenuItem::make()
-                ->icon('heroicon-o-collection')
-                ->label('Horizon')
-                ->url('/horizon'),
-        ];
     }
 }
